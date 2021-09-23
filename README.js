3a02e2eb08448df5c91026f9742db8c5ebbfd7c1
risbond <47916734+risbond@users.noreply.github.com>
2021-09-23 09:19:37 +0300

    Create README.js

---
+++ README.js
@@ -0,0 +1,18 @@
+import SoftwareDeveloper, { MyBiography } from 'risbond';
+
+export default class Bio extends SoftwareDeveloper {
+  name     = 'Samuel Risbond Lugona';
+  title    = 'Software Developer';
+  company  = 'Luris Systems';
+  location = 'Kampala, Uganda';
+  
+  getDetails(): MyBiography {
+    return {
+      "- ⚡ Quick bio:":                    "Quality oriented self motivating developer that is commercially-ware, evolving and enthusiastic about creating things",
+      "- 🔭 I’m currently working at":      `${this.company} as ${this.title}`,
+      "- 👯 I’m looking to collaborate on": "Node, React, PHP, Python and any other Javascript related projects",
+      "- 💬 Ask me about":                  "Node, React, PHP, SQL, CSS, Software Design & Architecture, Web-Development and any thing Photoshop related",
+      "- 📫 How to reach me:":              "samrisbond@gmail.com",
+    };
+  }
+}
